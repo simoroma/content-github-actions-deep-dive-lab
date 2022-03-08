0a9eecd3b0ea28abfd340304ac054f920c06d520
Simone Romano <me@simoneromano.com>
2022-03-08 15:58:12 +0200

    Mod

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ const IndexPage = () => {
 
           <h1 className='title'>
             Welcome to{' '}
-            <code className='title_code'>Cloud Coffee</code>
+            <code className='title_code'>Cloud Coffee NEWWW</code>
           </h1>
       
 
